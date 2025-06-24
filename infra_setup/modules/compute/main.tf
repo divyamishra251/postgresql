00102606ef57630233fc6dd3158675e2ff8923be
divya <divya@pandey2510divya@gmail.com>
2025-06-24 17:36:57 +0530

    Jenkinsfile for Ansible

--- infra_setup/modules/compute/main.tf
+++ infra_setup/modules/compute/main.tf
@@ -6,9 +6,37 @@ resource "aws_instance" "public" {
   subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [var.public_sg_id, var.jenkins_sg_id]
 
-
   tags = {
     Name = "${var.project_name}-public-instance"
+  }
+
+  # Provisioner to copy PEM file to Jenkins EC2 instance
+  provisioner "file" {
+    source      = "/home/divya/Downloads/apsouth.pem"
+    destination = "/tmp/apsouth.pem"
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("/home/divya/Downloads/apsouth.pem")
+      host        = self.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo mv /tmp/apsouth.pem /var/lib/jenkins/apsouth.pem",
+      "sudo cp /var/lib/jenkins/apsouth.pem /home/ubuntu/oneclick_infra/apsouth.pem",
+      "sudo chown jenkins:jenkins /var/lib/jenkins/apsouth.pem",
+      "sudo chmod 400 /var/lib/jenkins/apsouth.pem"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("/home/divya/Downloads/apsouth.pem")
+      host        = self.public_ip
+    }
   }
 }
 
@@ -20,7 +48,6 @@ resource "aws_instance" "private_1" {
   subnet_id              = var.private_subnet_id
   vpc_security_group_ids = [var.private_sg_id, var.jenkins_sg_id]
 
-  
   tags = {
     Name = "${var.project_name}-private-instance-1"
   }
@@ -33,7 +60,6 @@ resource "aws_instance" "private_2" {
   key_name               = var.key_name != "" ? var.key_name : null
   subnet_id              = var.private_subnet_id
   vpc_security_group_ids = [var.private_sg_id, var.jenkins_sg_id]
-
 
   tags = {
     Name = "${var.project_name}-private-instance-2"
