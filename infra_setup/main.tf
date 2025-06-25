b91b7120cb0e4492f68e4fa37fe9d0ff867472bc
divya <divya@pandey2510divya@gmail.com>
2025-06-25 13:11:50 +0530

    Jenkinsfile for Ansible

--- infra_setup/main.tf
+++ infra_setup/main.tf
@@ -12,22 +12,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Data source for the latest Amazon Linux 2 AMI
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 # Network Module
 module "network" {
   source = "./modules/network"
